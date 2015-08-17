3976f5694d6e143756a77a68d1bdba9e6bf33424
Zhang Xinyu <iamzhangxinyu@gmail.com>
2015-08-17 20:30:45 +0800

    server

---
+++ server.sh
@@ -0,0 +1,32 @@
+#!./node_modules/mocha/bin/mocha
+
+var dgram = require('dgram');
+var assert = require("assert");
+var message = new Buffer("status");
+var server = dgram.createSocket("udp4");
+var client = dgram.createSocket("udp4");
+
+var REMOTE = "10.29.2.234";
+var selfPort = 41234;
+var remotePort = 41235;
+
+server.bind(selfPort, 'localhost');
+
+describe('client', function () {
+
+    before(function () {
+
+        client.send("status", 0, "status".length, remotePort, REMOTE, function (err) {
+            client.close();
+        });
+    });
+
+    it('should report led status', function (done) {
+        server.on("message", function (msg, rinfo) {
+            console.log('msg: ' + msg);
+            assert.equal("1", msg);
+            done();
+            server.close();
+        });
+    });
+});
